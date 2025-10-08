f1ecf0735b9b0939a4b5197ef5ebf33b6129b8c6
Seamus Kelly <seamus@keldo.uk>
2025-10-08 17:11:39 +0100

    Remove example TCP monitor from uptime.config.ts
    Removed example TCP monitor configuration from uptime settings.

--- uptime.config.ts
+++ uptime.config.ts
@@ -70,18 +70,6 @@ const workerConfig: WorkerConfig = {
       // [OPTIONAL] if true, the check will fallback to local if the specified proxy is down
       checkProxyFallback: true,
     },
-    // Example TCP Monitor
-    {
-      id: 'test_tcp_monitor',
-      name: 'Example TCP Monitor',
-      // `method` should be `TCP_PING` for tcp monitors
-      method: 'TCP_PING',
-      // `target` should be `host:port` for tcp monitors
-      target: '1.2.3.4:22',
-      tooltip: 'My production server SSH',
-      statusPageLink: 'https://example.com',
-      timeout: 5000,
-    },
   ],
   notification: {
     // [Optional] apprise API server URL
