342f73bf67c745f6e61c3f97cac8fd3e85ff4971
Seamus Kelly <seamus@keldo.uk>
2025-10-08 18:25:49 +0100

    Comment out maintenance config entries
    Comment out maintenance configuration details for clarity.

--- uptime.config.ts
+++ uptime.config.ts
@@ -127,18 +127,18 @@ const workerConfig: WorkerConfig = {
 const maintenances: MaintenanceConfig[] = [
   {
     // [Optional] Monitor IDs to be affected by this maintenance
-    monitors: ['vault', 'bar_monitor'],
+    //monitors: ['vault', 'bar_monitor'],
     // [Optional] default to "Scheduled Maintenance" if not specified
-    title: 'Test Maintenance',
+    //title: 'Test Maintenance',
     // Description of the maintenance, will be shown at status page
-    body: 'This is a test maintenance, server software upgrade',
+    //body: 'This is a test maintenance, server software upgrade',
     // Start time of the maintenance, in UNIX timestamp or ISO 8601 format
-    start: '2025-04-27T00:00:00+08:00',
+    //start: '2025-04-27T00:00:00+08:00',
     // [Optional] end time of the maintenance, in UNIX timestamp or ISO 8601 format
     // if not specified, the maintenance will be considered as on-going
-    end: '2025-04-30T00:00:00+08:00',
+    //end: '2025-04-30T00:00:00+08:00',
     // [Optional] color of the maintenance alert at status page, default to "yellow"
-    color: 'blue',
+    //color: 'blue',
   },
   // As this config file is a TypeScript file, you can even use IIFE to generate scheduled maintenances
   // The following example shows a scheduled maintenance from 2 AM to 4 AM on the 15th of every month (UTC+8)
